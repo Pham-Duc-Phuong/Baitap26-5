eba4032a3643552530fb287180a3835606525173
Pham Duc Phuong <phuong.mom33@gmail.com>
2023-05-27 23:08:21 +0700

    ss

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@
 // 
 // OUTPUT 
 // kết quả
-var parameter = [];
+var parameter = ["4", "13", "-1", "-13", "-3", "-4"];
 document.getElementById("btn0").onclick = function () {
     var num = document.getElementById("num").value;
     parameter.push(num);
@@ -54,7 +54,7 @@ document.getElementById("btn4").onclick = function () {
     var min = parameter[0];
     for (var j = 0; j < parameter.length; j++) {
         var value = +parameter[j]
-        if (+parameter[j]>0 && value < min) {
+        if (+parameter[j] > 0 && value < min) {
             min = value
         }
     }
@@ -63,7 +63,7 @@ document.getElementById("btn4").onclick = function () {
 document.getElementById("btn5").onclick = function () {
     var lastEven = -1;
     for (var j = parameter.length - 1; j >= 0; j--) {
-        var value = parameter[j]
+        var value = parameter[j];
         if (value % 2 === 0) {
             lastEven = value
             break
@@ -71,8 +71,42 @@ document.getElementById("btn5").onclick = function () {
     }
     document.getElementById("demo5").innerHTML = "Số chẵn cuối cùng: " + lastEven;
 }
-// document.getElementById("btn6").onclick = function () {}
-// document.getElementById("btn7").onclick = function () {}
-// document.getElementById("btn8").onclick = function () {}
-// document.getElementById("btn9").onclick = function () {}
+document.getElementById("btn6").onclick = function () {
+    var i = document.getElementById("change1").value;
+    var j = document.getElementById("change2").value;
+    var a = parameter[i - 1];
+    var b = parameter[j - 1]
+    parameter[i - 1] = b
+    parameter[j - 1] = a
+    document.getElementById("demo6").innerHTML = "Chuối số: " + parameter.join(", ");
+}
+document.getElementById("btn7").onclick = function () {
+    parameter.sort(function sortt(x, y) { return x - y });
+    document.getElementById("demo7").innerHTML = "Sắp xếp mảng theo thứ tự tăng dần: " + parameter.join(", ");;
+}
+document.getElementById("btn8").onclick = function () {
+    for (var i = 0; i < parameter.length; i++) {
+        if (parameter[i]= 2) {
+            num = parameter[i];
+            break
+        } else if (num > 2) {
+            for (j = 2; j < parameter[i]; j++) {
+                if (num % j !== 0){
+                    num = parameter[i]
+                    break
+                } else {
+                    num = "Không tìm thấy số nguyên tố"
+                }
+            }
+        }
+    }
+    document.getElementById("demo8").innerHTML = "Số nguyên tố đầu tiên: " + num;
+}
+document.getElementById("btn9").onclick = function () {
+    for (var i = 0; i < parameter.length; i++) {
+        console.log(Number.isInteger(parameter[2])); 
+        
+        
+}
+}
 // document.getElementById("btn10").onclick = function () {}
