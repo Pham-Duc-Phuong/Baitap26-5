f9ac59d023e27bb53e6ae1360059969c2d49f05b
Pham Duc Phuong <phuong.mom33@gmail.com>
2023-05-28 05:22:46 +0700

    ss

--- js/index.js
+++ js/index.js
@@ -22,6 +22,7 @@ document.getElementById("btn0").onclick = function () {
     parameter.push(num);
     document.getElementById("string1").innerHTML = "Chuối số: " + parameter.join(", ");
 };
+// BÀI 1 BÀI 1 BÀI 1 BÀI 1 BÀI 1 BÀI 1 BÀI 1 BÀI 1 
 document.getElementById("btn1").onclick = function () {
     var sum = 0;
     for (var i = 0; i < parameter.length; i++) {
@@ -31,6 +32,7 @@ document.getElementById("btn1").onclick = function () {
     };
     document.getElementById("demo1").innerHTML = "Tổng các số dương: " + sum;
 };
+// BÀI 2 BÀI 2 BÀI 2 BÀI 2 BÀI 2 BÀI 2 BÀI 2 BÀI 2  
 document.getElementById("btn2").onclick = function () {
     var sum = 0;
     for (var j = 0; j < parameter.length; j++) {
@@ -40,6 +42,7 @@ document.getElementById("btn2").onclick = function () {
     }
     document.getElementById("demo2").innerHTML = "Đếm số dương: " + sum;
 }
+// BÀI 3  BÀI 3 BÀI 3 BÀI 3 BÀI 3 BÀI 3 BÀI 3 BÀI 3
 document.getElementById("btn3").onclick = function () {
     var min = parameter[0];
     for (var j = 0; j < parameter.length; j++) {
@@ -50,6 +53,7 @@ document.getElementById("btn3").onclick = function () {
     }
     document.getElementById("demo3").innerHTML = "Số nhỏ nhất: " + min;
 }
+// BÀI 4  BÀI 4 BÀI 4 BÀI 4 BÀI 4 BÀI 4 BÀI 4 BÀI 4
 document.getElementById("btn4").onclick = function () {
     var min = parameter[0];
     for (var j = 0; j < parameter.length; j++) {
@@ -60,6 +64,7 @@ document.getElementById("btn4").onclick = function () {
     }
     document.getElementById("demo4").innerHTML = "Số dương nhỏ nhất: " + min;
 }
+// BÀI 5  BÀI 5 BÀI 5 BÀI 5 BÀI 5 BÀI 5 BÀI 5 BÀI 5
 document.getElementById("btn5").onclick = function () {
     var lastEven = -1;
     for (var j = parameter.length - 1; j >= 0; j--) {
@@ -71,6 +76,7 @@ document.getElementById("btn5").onclick = function () {
     }
     document.getElementById("demo5").innerHTML = "Số chẵn cuối cùng: " + lastEven;
 }
+// BÀI 6 BÀI 6 BÀI 6 BÀI 6 BÀI 6 BÀI 6 BÀI 6 BÀI 6
 document.getElementById("btn6").onclick = function () {
     var i = document.getElementById("change1").value;
     var j = document.getElementById("change2").value;
@@ -80,28 +86,34 @@ document.getElementById("btn6").onclick = function () {
     parameter[j - 1] = a
     document.getElementById("demo6").innerHTML = "Chuối số: " + parameter.join(", ");
 }
+// BÀI 7  BÀI 7 BÀI 7 BÀI 7 BÀI 7 BÀI 7 BÀI 7 BÀI 7
 document.getElementById("btn7").onclick = function () {
-    parameter.sort(function sortt(x, y) { return x - y });
-    document.getElementById("demo7").innerHTML = "Sắp xếp mảng theo thứ tự tăng dần: " + parameter.join(", ");;
+    var sortxy = parameter;
+    sortxy.sort(function sortt(x, y) { return x - y });
+    document.getElementById("demo7").innerHTML = "Sắp xếp mảng theo thứ tự tăng dần: " + sortxy.join(", ");;
 }
+// BÀI 8  BÀI 8 BÀI 8 BÀI 8 BÀI 8 BÀI 8 BÀI 8 BÀI 8
 document.getElementById("btn8").onclick = function () {
     for (var i = 0; i < parameter.length; i++) {
-        if (parameter[i]= 2) {
-            num = parameter[i];
+        debugger;
+        var num = +parameter[i];
+        if (num === 2) {
+            num = 2;
             break
-        } else if (num > 2) {
-            for (j = 2; j < parameter[i]; j++) {
-                if (num % j !== 0){
-                    num = parameter[i]
-                    break
-                } else {
+        } else if (num > 2 && parameter[i] > 0) {
+            for (j = 2; j < num; j++) {
+                if (num % j === 0){
                     num = "Không tìm thấy số nguyên tố"
+                } else {
+                    num = parameter[i]
                 }
-            }
+            }  
         }
+        
     }
     document.getElementById("demo8").innerHTML = "Số nguyên tố đầu tiên: " + num;
 }
+// BÀI 9  BÀI 9 BÀI 9 BÀI 9 BÀI 9 BÀI 9 BÀI 9 BÀI 9
 document.getElementById("btn9").onclick = function () {
     for (var i = 0; i < parameter.length; i++) {
         console.log(Number.isInteger(parameter[2])); 
@@ -109,4 +121,5 @@ document.getElementById("btn9").onclick = function () {
         
 }
 }
+// BÀI 10 BÀI 10 BÀI 10 BÀI 10 BÀI 10 BÀI 10 BÀI 10
 // document.getElementById("btn10").onclick = function () {}
