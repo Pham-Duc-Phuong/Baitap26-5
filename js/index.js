05b39451b1308020ff92707301212aaff2c6c104
Pham Duc Phuong <phuong.mom33@gmail.com>
2023-05-28 07:43:10 +0700

    ss

--- js/index.js
+++ js/index.js
@@ -1,22 +1,4 @@
-// // INPUT
-// chuỗi 
-// Xử lý 
-// Nhập số
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// OUTPUT 
-// kết quả
-var parameter = ["4", "13", "-1", "-13", "-3", "-4"];
+var parameter = [];
 document.getElementById("btn0").onclick = function () {
     var num = document.getElementById("num").value;
     parameter.push(num);
@@ -44,8 +26,9 @@ document.getElementById("btn2").onclick = function () {
 }
 // BÀI 3  BÀI 3 BÀI 3 BÀI 3 BÀI 3 BÀI 3 BÀI 3 BÀI 3
 document.getElementById("btn3").onclick = function () {
-    var min = parameter[0];
+    var min = +parameter[0];
     for (var j = 0; j < parameter.length; j++) {
+        debugger;
         var value = +parameter[j]
         if (value < min) {
             min = value
@@ -55,11 +38,13 @@ document.getElementById("btn3").onclick = function () {
 }
 // BÀI 4  BÀI 4 BÀI 4 BÀI 4 BÀI 4 BÀI 4 BÀI 4 BÀI 4
 document.getElementById("btn4").onclick = function () {
-    var min = parameter[0];
+    var min = parameter[0]
     for (var j = 0; j < parameter.length; j++) {
-        var value = +parameter[j]
-        if (+parameter[j] > 0 && value < min) {
-            min = value
+        var a = parameter[j]
+        if (min <= 0){
+            min = parameter[j+1]
+        } else if (a > 0 && a < min) {
+            min = a
         }
     }
     document.getElementById("demo4").innerHTML = "Số dương nhỏ nhất: " + min;
@@ -95,17 +80,17 @@ document.getElementById("btn7").onclick = function () {
 // BÀI 8  BÀI 8 BÀI 8 BÀI 8 BÀI 8 BÀI 8 BÀI 8 BÀI 8
 document.getElementById("btn8").onclick = function () {
     for (var i = 0; i < parameter.length; i++) {
-        debugger;
+        // debugger;
         var num = +parameter[i];
         if (num === 2) {
             num = 2;
             break
-        } else if (num > 2 && parameter[i] > 0) {
+        } else if (num > 2 && +parameter[i] > 0) {
             for (j = 2; j < num; j++) {
                 if (num % j === 0){
                     num = "Không tìm thấy số nguyên tố"
                 } else {
-                    num = parameter[i]
+                    num = +parameter[i]
                 }
             }  
         }
@@ -114,12 +99,31 @@ document.getElementById("btn8").onclick = function () {
     document.getElementById("demo8").innerHTML = "Số nguyên tố đầu tiên: " + num;
 }
 // BÀI 9  BÀI 9 BÀI 9 BÀI 9 BÀI 9 BÀI 9 BÀI 9 BÀI 9
-document.getElementById("btn9").onclick = function () {
+document.getElementById("btn9").onclick = function () {    
+    var sum = 0
     for (var i = 0; i < parameter.length; i++) {
-        console.log(Number.isInteger(parameter[2])); 
-        
-        
+        var a = Number.isInteger(+parameter[i])
+        if (a === true){
+            sum += 1
+        }  
 }
+document.getElementById("demo9").innerHTML = "Số lượng số nguyên: " + sum;
 }
 // BÀI 10 BÀI 10 BÀI 10 BÀI 10 BÀI 10 BÀI 10 BÀI 10
-// document.getElementById("btn10").onclick = function () {}
+document.getElementById("btn10").onclick = function () {
+    var sumPosi = 0;
+    var sumNegav = 0;
+    for ( var i = 0 ; i < parameter.length; i++){
+        if (+parameter[i] > 0){
+            sumPosi += 1;
+        } else {
+            sumNegav += 1;
+        }
+    }
+    if (sumPosi > sumNegav){
+        document.getElementById("demo10").innerHTML = "số dương: "+ sumPosi + " > " + "số âm: " + sumNegav;
+    } else {
+        document.getElementById("demo10").innerHTML = "số dương: "+ sumPosi + " < " + "số âm: " + sumNegav;
+    }
+    
+}
